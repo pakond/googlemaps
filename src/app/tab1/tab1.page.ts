02e5ee6fee22fa8a25500e959cb644300f208967
DESKTOP-IRJK59P\jrbis <jrbistuer@gmail.com>
2021-06-23 10:18:08 +0200

    maps

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { ToastController } from '@ionic/angular';
+
+declare const google;
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  @ViewChild('map', { static: false }) mapElement: ElementRef;
+  map: any;
 
-  constructor() {}
+  constructor(public toastController: ToastController) {}
 
+  ionViewDidEnter() {
+    this.getLocation();
+  }
+
+  async getLocation() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.presentToast(
+      coordinates.coords.latitude.toString() +
+        coordinates.coords.longitude.toString()
+    );
+    this.loadMap(coordinates);
+  }
+
+  async presentToast(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+  loadMap(coordinates) {
+    const latLng = new google.maps.LatLng(
+      coordinates.coords.latitude,
+      coordinates.coords.longitude
+    );
+    const mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.addMarker(this.map);
+  }
+
+  addMarker(map) {
+    const marker = new google.maps.Marker({
+      map,
+      animation: google.maps.Animation.DROP,
+      position: map.getCenter(),
+    });
+  }
 }
